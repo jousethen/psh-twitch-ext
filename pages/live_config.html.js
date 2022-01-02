ff4af36bc73dc766c7fa22b1d95f9446b918c2a8
jousethen <jthen93@gmail.com>
2022-01-02 15:58:34 -0500

    query js will now just hold the queries. Fetching done using async/await

--- pages/live_config.html.js
+++ pages/live_config.html.js
@@ -1,31 +1,38 @@
 import Layout from "../components/Layout";
 import React, { useState, useEffect } from 'react'
 import TournamentSearch from "../components/TournamentSearch";
+import { fetchPlayers } from "../lib/queries";
 
 const LiveConfig = props => {
   const [tournamentSlug, setTournamentSlug] = useState('')
   const [allPlayers, setAllPlayers] = useState([])
 
-  const onTournamentSubmit = async (e, link) => {
+  const onTournamentSubmit = async (e, slug) => {
     e.preventDefault()
+    console.log(process.env)
     //Get Tournament Slug From URL
-    link = link.substring(link.indexOf("tournament/") + 11)
+    slug = slug.substring(slug.indexOf("tournament/") + 11)
     //Delete everything after the slug
-    link = link.split("/")[0]
-    console.log(link)
+    slug = slug.split("/")[0]
 
-    //Query SGG for Players(Maybe Put Fetch Into Another File?)
-    fetch('https://api.smash.gg/gql/alpha', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': "Bearer #{@token}"
-      },
-      body: JSON.stringify({
-        query: q,
-        variables: variables
-      })
-    });
+    const [query, variables] = fetchPlayers(slug)
+    const res = await fetch(
+      process.env.APIURL,
+      {
+        body: JSON.stringify({
+          query: query,
+          variables: variables
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.SMASHGGKEY}`
+        },
+        method: 'POST'
+      }
+    )
+
+    const result = await res.json()
+    // result.user => 'Ada Lovelace'
 
     //Set State with all players so it can be passed to component allowing you to choose Max Two players
   }
