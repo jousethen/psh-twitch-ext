6b5d3c1558aba51641224a4b11ac3b20ac823881
jousethen <jthen93@gmail.com>
2022-01-18 15:54:38 -0500

    Successfully pulling top item on stream queue and displaying it

--- components/PlayerContainer.jsx
+++ components/PlayerContainer.jsx
@@ -1,14 +1,39 @@
 import React, { useState } from 'react'
+import { fetchPlayers } from '../lib/queries'
 
 const PlayerContainer = (props) => {
-  const [searchText, setSearchText] = useState('')
   const [player1, setPlayer1] = useState(props.players[0].entrant)
   const [player2, setPlayer2] = useState(props.players[1].entrant)
+
+  const refresh = async () => {
+    const [query, variables] = fetchPlayers(props.slug)
+    const res = await fetch(
+      process.env.APIURL,
+      {
+        body: JSON.stringify({
+          query: query,
+          variables: variables
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.SMASHGGKEY}`
+        },
+        method: 'POST'
+      }
+    )
+
+    const result = await res.json()
+
+    console.log(result.data.tournament.streamQueue[0].sets[0].slots)
+    //Set State with all players so it can be passed to component allowing you to choose Max Two players
+    setPlayer1(result.data.tournament.streamQueue[0].sets[0].slots[0].entrant);
+    setPlayer2(result.data.tournament.streamQueue[0].sets[0].slots[1].entrant);
+  }
 
   return (
     <>
       <h3>{player1.name} <span style={{ color: "red" }}>VS</span> {player2.name}</h3>
-
+      <button className="btn btn-outline-info" onClick={refresh}>Refresh</button>
 
 
     </>
